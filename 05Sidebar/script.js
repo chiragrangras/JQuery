588fa449d80ca25ebf25c1e6bc0d207b4e139185
Chirag Rangras <chiragrangras012@gmail.com>
2023-11-01 22:40:41 +0530

    new file

--- 05Sidebar/script.js
+++ 05Sidebar/script.js
@@ -10,11 +10,16 @@ $(document).ready(function () {
   $("#dark").click(function () {
     $("#nav-bar").toggleClass("dark-theme");
     $(".nav a").toggleClass("dark-theme");
+    $(".nav").toggleClass("dark-theme-border-bottom");
     $("#left").toggleClass("dark-theme");
     $("#right").toggleClass("dark-theme");
     $("#left-container a").toggleClass("dark-theme");
     $("#acc-container").toggleClass("dark-theme");
-    $("#content").toggleClass('dark-theme');
+    $("#content").toggleClass('dark-theme-content');
+    $("#content").toggleClass('dark-theme-border-lr');
+    $("body").toggleClass('dark-theme');
+    $("#acc-btn").toggleClass('dark-theme');
+    $("#drop-btn").toggleClass('dark-theme');
   });
 
   $("#acc-btn").hover(
@@ -25,4 +30,12 @@ $(document).ready(function () {
       $("#demo-acc").hide();
     }
   );
+  $("#drop-btn").hover(
+    function () {
+      $("#down-acc").show();
+    },
+    function () {
+      $("#down-acc").hide();
+    }
+  )
 });
