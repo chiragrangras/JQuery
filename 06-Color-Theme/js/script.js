fca9a49d0756b24ead69ffa46d53d9e16fa685b6
Chirag Rangras <chiragrangras012@gmail.com>
2023-11-09 23:04:27 +0530

    new file

--- 06-Color-Theme/js/script.js
+++ 06-Color-Theme/js/script.js
@@ -1,11 +1,3 @@
-function showAcc() {
-  $("#demo-acc").show();
-}
-
-function hideAcc() {
-  $("#demo-acc").hide();
-}
-
 $(document).ready(function () {
   
   $("#acc-btn").hover(
@@ -16,6 +8,7 @@ $(document).ready(function () {
       $("#demo-acc").hide();
     }
   );
+
   $("#drop-btn").hover(
     function () {
       $("#down-acc").show();
@@ -24,4 +17,49 @@ $(document).ready(function () {
       $("#down-acc").hide();
     }
   );
+
+  $("#themes-selector").change(function (){
+    
+    switch(this.value){
+      case 'white':
+        console.log("You selected white theme"); 
+        $('body').attr("class", "theme-white");
+        $('#main-container').attr("class", "theme-white");
+        break;
+      case 'orange':
+        console.log("You selected orange theme");
+        $('body').attr("class", "theme-orange");
+        $('#main-container').attr("class", "theme-orange");
+        break;
+      case 'green':
+        console.log("You selected green theme");
+        $('body').attr("class","theme-green");
+        $('#main-container').attr("class","theme-green");
+        break;
+      case 'blue':
+        console.log("You selected blue theme");
+        $('body').attr("class","theme-blue");
+        $('#main-container').attr("class","theme-blue");
+        break;
+      case 'purple':
+        console.log("You selected purple theme");
+        $('body').attr("class","theme-purple");
+        $('#main-container').attr("class","theme-purple");
+        break;
+      case 'black':
+        console.log("You selected black theme");
+        $('body').attr("class","theme-black");
+        $('#main-container').attr("class","theme-black");
+        break;
+      default:
+        console.log("Invalid theme selection.");
+    }
+
+    // if(this.value === 'white'){
+    //   console.log("You selected white theme");
+    // }
+    // else if(this.value === 'orange'){
+    //   console.log("You selected orange theme");
+    // }
+  });
 });
