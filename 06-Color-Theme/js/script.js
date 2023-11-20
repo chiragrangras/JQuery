8af4a3dd0fa030453056c407483b1b2f195f8c02
Chirag Rangras <chiragrangras012@gmail.com>
2023-11-20 21:45:21 +0530

    new file

--- 06-Color-Theme/js/script.js
+++ 06-Color-Theme/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  
   $("#acc-btn").hover(
     function () {
       $("#demo-acc").show();
@@ -18,48 +17,40 @@ $(document).ready(function () {
     }
   );
 
-  $("#themes-selector").change(function (){
-    
-    switch(this.value){
-      case 'white':
-        console.log("You selected white theme"); 
-        $('body').attr("class", "theme-white");
-        $('#main-container').attr("class", "theme-white");
+  $("#themes-selector").change(function () {
+    switch (this.value) {
+      case "white":
+        console.log("You selected white theme");
+        $("body").attr("class", "theme-white");
+        $("#main-container").attr("class", "theme-white");
         break;
-      case 'orange':
+      case "orange":
         console.log("You selected orange theme");
-        $('body').attr("class", "theme-orange");
-        $('#main-container').attr("class", "theme-orange");
+        $("body").attr("class", "theme-orange");
+        $("#main-container").attr("class", "theme-orange");
         break;
-      case 'green':
+      case "green":
         console.log("You selected green theme");
-        $('body').attr("class","theme-green");
-        $('#main-container').attr("class","theme-green");
+        $("body").attr("class", "theme-green");
+        $("#main-container").attr("class", "theme-green");
         break;
-      case 'blue':
+      case "blue":
         console.log("You selected blue theme");
-        $('body').attr("class","theme-blue");
-        $('#main-container').attr("class","theme-blue");
+        $("body").attr("class", "theme-blue");
+        $("#main-container").attr("class", "theme-blue");
         break;
-      case 'purple':
+      case "purple":
         console.log("You selected purple theme");
-        $('body').attr("class","theme-purple");
-        $('#main-container').attr("class","theme-purple");
+        $("body").attr("class", "theme-purple");
+        $("#main-container").attr("class", "theme-purple");
         break;
-      case 'black':
+      case "black":
         console.log("You selected black theme");
-        $('body').attr("class","theme-black");
-        $('#main-container').attr("class","theme-black");
+        $("body").attr("class", "theme-black");
+        $("#main-container").attr("class", "theme-black");
         break;
       default:
         console.log("Invalid theme selection.");
     }
-
-    // if(this.value === 'white'){
-    //   console.log("You selected white theme");
-    // }
-    // else if(this.value === 'orange'){
-    //   console.log("You selected orange theme");
-    // }
   });
 });
